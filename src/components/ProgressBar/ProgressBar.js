1399f227673ecd5940f63986498d874c470ed09c
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-10-27 21:51:20 -0400

    benhickson: feat: add ProgressBar to LearningModule

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Styles.scss';
+
+const ProgressBar = ({text, completedPercent}) => (
+  <div className="progressBar">
+    <div className="progressBar--unfilledBar">
+      <div className={ `progressBar--fill${
+        completedPercent === 100 ? ' progressBar--fill--completed' : '' 
+      }` } style={ {width: `${completedPercent}%`} }>
+        <span>{ text }</span>
+      </div>
+    </div>
+  </div>
+);
+
+ProgressBar.propTypes = {
+  text: PropTypes.string,
+  completedPercent: PropTypes.number,
+};
+
+ProgressBar.defaultProps = {
+  text: '45% Complete',
+  completedPercent: 45,
+};
+
+export default ProgressBar;
