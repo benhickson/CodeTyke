1bd38e9c57bf592fffb69c6dc434727014d1b5f0
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-11-19 00:15:37 -0500

    feat: add state management and submission handling to LearningModule

--- src/components/learningModule/LearningModule.js
+++ src/components/learningModule/LearningModule.js
@@ -13,6 +13,9 @@ const LearningModule = ({setGameStatus}) => {
   const [quizData, setQuizData] = React.useState({});
   const [showLoader, setShowLoader] = React.useState(false);
   const [showModal, setShowModal] = React.useState(false);
+  const [selectedAnswers, setSelectedAnswers] = React.useState([]);
+  const [incorrectAnswers, setIncorrectAnswers] = React.useState([]);
+  const [submissionComplete, setSubmissionComplete] = React.useState(false);
 
   let currentQuestion = quizData.questionArr ? quizData.questionArr[currentQuestionId]: {};
   React.useEffect(()=>{
@@ -30,23 +33,60 @@ const LearningModule = ({setGameStatus}) => {
       });
   }
 
-  const handleSubmit=()=> {
-    if(currentQuestionId < quizData.totalQuestions-1){
-      setShowLoader(true);
-      setTimeout(function(){
-        console.log("Checking answer...");
-        setCurrentQuestionId(currentQuestionId+1);
-        setShowLoader(false);
-      }, 500 );
-    } else {
-      setCurrentQuestionId(0);
-      setGameStatus({message: "Great Job! Play again.", loadIntro: true});
-    }
+  const handleSubmitOrNext=()=> {
+    setShowLoader(true);
+    setTimeout(function(){
+      setIncorrectAnswers([]);
+      setShowLoader(false);
+      if (!submissionComplete) {
+        if (submissionIsCorrect(currentQuestion, selectedAnswers)) {
+          setSubmissionComplete(true);
+        } else {
+          setIncorrectAnswers(selectedAnswers);
+          setSelectedAnswers([]);
+        } 
+      } else {
+        setSelectedAnswers([]);
+        setSubmissionComplete(false);
+        if(currentQuestionId < quizData.totalQuestions-1){
+          setCurrentQuestionId(currentQuestionId+1);
+        } else {
+          setCurrentQuestionId(0);
+          setGameStatus({message: "Great Job! Play again.", loadIntro: true});
+        }
+      }
+    }, 500 );
   }
+
+  const submissionIsCorrect = (currentQuestion, selectedAnswers) => {
+    for (let index = 0; index < currentQuestion.possibleAnswers.length; index++) {
+      if (currentQuestion.possibleAnswers[index].isCorrect !== selectedAnswers.includes(index)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  const handleCheckboxClick = (index) => {
+    const newState = [...selectedAnswers];
+    const indexIdx = newState.indexOf(index);
+    if (indexIdx < 0) {
+      newState.push(index);
+    } else {
+      newState.splice(indexIdx, 1);
+    }
+    setSelectedAnswers(newState);
+  };
+
   let possibleAnswers = [];
   if(currentQuestion.possibleAnswers){
     possibleAnswers = currentQuestion.possibleAnswers.map((answer, index) => {
-      return <SelectionBox id={index} key={index} answer={answer} />
+      return <SelectionBox id={index} key={index} answer={answer}
+        checked={selectedAnswers.includes(index)}
+        incorrect={incorrectAnswers.includes(index)}
+        click={submissionComplete ? () => null : handleCheckboxClick}
+        locked={submissionComplete}
+      />
     })
   }
 
@@ -80,7 +120,7 @@ const LearningModule = ({setGameStatus}) => {
               { possibleAnswers }
             </div>
             <div className="learningModule--submitButtonContainer">
-              <Button label="Submit" handleSubmit={ handleSubmit } showLoader={showLoader} hasIcons />
+              <Button label={submissionComplete ? 'Next' : 'Submit'} handleSubmit={handleSubmitOrNext} showLoader={showLoader} hasIcons enabled={selectedAnswers.length > 0} />
             </div>
           </div>
         </>
--- src/components/selectionBox/SelectionBox.js
+++ src/components/selectionBox/SelectionBox.js
@@ -3,13 +3,20 @@ import React from 'react';
 import './Styles.scss';
 
 const SelectionBox = (props) => {
+  // Note for class: We had to break from the && pattern because ("submitButton" + false) yields "submitButtonfalse"
+  const classes = ['selectionBox'];
+  if (props.checked)    classes.push('checked');
+  if (props.incorrect)  classes.push('incorrect');
+  if (props.locked)     classes.push('locked');
+
   return(
     <div
-      className="selectionBox"
+      className={classes.join(' ')}
       id={"selectionBox" + props.id}
+      onClick={() => props.click(props.id)}
     >
       <img className="selectionBox--image" alt={props.answer.imageAlt} src={props.answer.image} />
-      <input className="selectionBox--checkbox" type="checkbox" />
+      <input className="selectionBox--checkbox" type="checkbox" checked={props.checked} onChange={() => null} />
       <span className="selectionBox--text">{props.answer.text}</span>
     </div>
    )
